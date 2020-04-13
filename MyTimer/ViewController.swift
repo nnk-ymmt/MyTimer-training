d923e7deb936acfc93fc867f093eaabfd22e8f5f
Nonoka Yamamoto <garden.n1234567890@gmail.com>
2020-04-13 20:29:20 +0900

    Initial Commit

---
+++ MyTimer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyTimer
+//
+//  Created by 山本ののか on 2020/04/13.
+//  Copyright © 2020 NonokaYamamoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
